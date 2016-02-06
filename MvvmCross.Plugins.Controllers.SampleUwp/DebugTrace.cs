da7624e77c7ca55323692cb5afee908c5914f756
Andres Rutnik <andres.rutnik@digiflare.com>
2016-02-06 10:58:03 -0500

    Added sample UWP app

---
+++ MvvmCross.Plugins.Controllers.SampleUwp/DebugTrace.cs
@@ -0,0 +1,35 @@
+﻿using MvvmCross.Platform.Platform;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MvvmCross.Plugins.Controllers.SampleUwp
+{
+    public class DebugTrace : IMvxTrace
+    {
+        public void Trace(MvxTraceLevel level, string tag, Func<string> message)
+        {
+            Debug.WriteLine(tag + ":" + level + ":" + message());
+        }
+
+        public void Trace(MvxTraceLevel level, string tag, string message)
+        {
+            Debug.WriteLine(tag + ":" + level + ":" + message);
+        }
+
+        public void Trace(MvxTraceLevel level, string tag, string message, params object[] args)
+        {
+            try
+            {
+                Debug.WriteLine(string.Format(tag + ":" + level + ":" + message, args));
+            }
+            catch (FormatException)
+            {
+                Trace(MvxTraceLevel.Error, tag, "Exception during trace of {0} {1} {2}", level, message);
+            }
+        }
+    }
+}
